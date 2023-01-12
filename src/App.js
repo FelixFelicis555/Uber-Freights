849b516778c3465ba5dde2ac67bff8aae51ee1b4
FelixFelicis555 <perfectpawi13@gmail.com>
2023-01-12 18:06:39 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import LoginForm from "./components/LoginForm/LoginForm"
+import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
+import Home from "./components/Home/Home";
+import PrivateRoute from "./utils/privateroute";
+import "boostrap/dist/css/bootstrap.min.css";
+import ViewPackage from "./components/Home/Packages";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, updateTitle] = useState(null);
+    const [errorMessage, updateErrorMessage] = useState(null);
+    return ( <div >
+        <Router >
+        <div className = "App" >
+        <
+        Header title = { title }
+        /> <div className = "container d-flex align-items-center flex-column" >
+        <Switch >
+        <Route path = "/"
+        exact = { true } >
+        <RegistrationForm showError = { updateErrorMessage }
+        updateTitle = { updateTitle }
+        /> </Route> 
+        <Route path = "/register" >
+        <
+        RegistrationForm showError = { updateErrorMessage }
+        updateTitle = { updateTitle }
+        /> </Route> <
+        Route path = "/login" >
+        <
+        LoginForm showError = { updateErrorMessage }
+        updateTitle = { updateTitle }
+        /> </Route> <
+        PrivateRoute path = "/home" >
+        <
+        Home / >
+        </PrivateRoute> <
+        PrivateRoute path = "/packages" >
+        < ViewPackage / >
+        </PrivateRoute>
+         </Switch> 
+        </div>
+         </div>
+         </Router>
+         </div>
+
+    );
 }
 
 export default App;
